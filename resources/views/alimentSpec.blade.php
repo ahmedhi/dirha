b2747152030ddb82fe3dbc3eb670ca4128b0fa5e
ahmedHi <ahmedhilali07@gmail.com>
2019-06-06 00:20:43 +0000

    fix img problem

--- resources/views/alimentSpec.blade.php
+++ resources/views/alimentSpec.blade.php
@@ -3,7 +3,7 @@
 </head>
 <div id="Value" class="row" style="align-content: center">
     <div class="col-2">
-        <img src="img/aliment/{{ $Aliment->nom }}.png" width="75px" height="75px">
+        <img src="img/Aliments/{{ $Aliment->nom }}.png" width="75px" height="75px">
     </div>
     <div class="col-10">
         <table class="table-borderless" style="width: 100%">
