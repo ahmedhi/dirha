1ed09fb2368c982dfbb9a74dab310467689cb953
ahmedHi <ahmedhilali07@gmail.com>
2019-06-08 22:28:14 +0000

    Fix some bugs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 
     <!-- Intro Section -->
-    <div class="hero-section">
+    <div class="hero-section" id="m">
         <div class="hero-content">
             <!--
             <div class="hero-center">
@@ -70,124 +70,55 @@
         <!-- card section end-->
 
 
-        <!-- About contant -->
-        <div class="about-contant">
+    <!-- Articles section -->
+        <div class="testimonial-section pb100">
+            <!-- <div class="test-overlay"></div> Image -->
             <div class="container">
-                <div class="section-title">
-                    <h2>Get in <span>the Lab</span> and discover the world</h2>
-                </div>
                 <div class="row">
-                    <div class="col-md-6">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Nulla sit amet luctus dolor. Etiam finibus consequat ante ac congue. Quisque porttitor porttitor tempus. Donec maximus ipsum non ornare vporttitor porttitorestibulum. Sed libero nibh, feugiat at enim id, bibendum sollicitudin arcu.</p>
-                    </div>
-                    <div class="col-md-6">
-                        <p>Cras ex mauris, ornare eget pretium sit amet, dignissim et turpis. Nunc nec maximus dui, vel suscipit dolor. Donec elementum velit a orci facilisis rutrum. Nam convallis vel erat id dictum. Sed ut risus in orci convallis viverra a eget nisi. Aenean pellentesque elit vitae eros dignissim ultrices. Quisque porttitor porttitorlaoreet vel risus et luctus.</p>
-                    </div>
-                </div>
-                <div class="text-center mt60">
-                    <a href="" class="site-btn">Browse</a>
-                </div>
-                <!-- popup video -->
-                <div class="intro-video">
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <img src="img/video.jpg" alt="">
-                            <a href="https://www.youtube.com/watch?v=JgHfx2v9zOU" class="video-popup">
-                                <i class="fa fa-play"></i>
-                            </a>
+                    <div class="col">
+                        <div class="section-title ">
+                            <h2>Derniers articles</h2>
+                        </div>
+                        <div class="owl-carousel" id="testimonial-slide">
+                                @foreach( $articles as $article)
+                                    <!-- single testimonial -->
+                                    <div class="testimonial">
+                                        <h2 style="text-align: center ; color: whitesmoke;" >{{ $article->title }}</h2>
+                                        <br>
+                                        <p style="height: 100px ; overflow: hidden ">{{ $article->description }}</p>
+                                        <br>
+                                        <a href="" class="justify-content-md-end">Voir plus ...</a>
+                                        <div class="client-info">
+                                            <div class="avatar">
+                                                <img src="UserImage/{{$article->users->img}}" alt="">
+                                            </div>
+                                            <div class="client-name">
+                                                <h2>{{$article->users->nom}}</h2>
+                                                <p class="right">{{$article->source}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- About section end -->
 
-
-    <!-- Testimonial section -->
-    <div class="testimonial-section pb100">
-        <div class="test-overlay"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-md-offset-4">
-                    <div class="section-title left">
-                        <h2>Les avis de nos clients</h2>
-                    </div>
-                    <div class="owl-carousel" id="testimonial-slide">
-                        <!-- single testimonial -->
-                        <div class="testimonial">
-                            <span>‘​‌‘​‌</span>
-                            <p>Je m'attendais pas à un résultat pareil. J'ai pu atteindre l'objectif que je désirai</p>
-                            <div class="client-info">
-                                <div class="avatar">
-                                    <img src="img/avatar/01.jpg" alt="">
-                                </div>
-                                <div class="client-name">
-                                    <h2>AFFACH Nahid</h2>
-                                    <p>Cliente depuis 2017</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Testimonial section end-->
-
-    <!-- Services section
-    <div class="services-section spad">
-        <div class="container">
-            <div class="section-title dark">
-                <h2>Get in <span>the Lab</span> and see the services</h2>
-            </div>
-            <div class="row">
-                single service
-                <div class="col-md-4 col-sm-6">
-                    <div class="service">
-                        <div class="icon">
-                            <i class="flaticon-023-flask"></i>
-                        </div>
-                        <div class="service-text">
-                            <h2>Get in the lab</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
-                        </div>
-                    </div>
-                </div>
-                single service
-                <div class="col-md-4 col-sm-6">
-                    <div class="service">
-                        <div class="icon">
-                            <i class="flaticon-011-compass"></i>
-                        </div>
-                        <div class="service-text">
-                            <h2>Projects online</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla..</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="text-center">
-                <a href="" class="site-btn">Browse</a>
-            </div>
-        </div>
-    </div>
-     services section end -->
-
-
+        <!-- Articles section end-->
 
     <!-- Team Section -->
     <div class="team-section spad">
         <div class="overlay"></div>
         <div class="container">
             <div class="section-title">
-                <h2>Rencontrer <span>FALLO</span> les fondateur</h2>
+                <h2>Rencontrer les <span>FALLO</span> Developers</h2>
             </div>
             <div class="row">
                 <!-- single member -->
                 <div class="col-sm-4">
                     <div class="member">
-                        <img src="img/team/dev3.jpg" alt="">
+                        <img src="img/team/dev3.jpg" alt="" style="">
                         <h2>HILALI Ahmed</h2>
                         <h3>Co-Fondateur</h3>
                     </div>
